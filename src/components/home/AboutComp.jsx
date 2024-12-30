efb90688624f14b58928877309ae95b032743258
iysaofficial <youngscientist.iysa@gmail.com>
2024-12-30 13:31:45 +0700

    about edit

--- src/components/home/AboutComp.jsx
+++ src/components/home/AboutComp.jsx
@@ -7,14 +7,12 @@ const AboutComp = () => {
                 <h1>IYEO</h1>
                 <h5>International Youth Economy Olympiad</h5>
                 <p>The Olympiad is a competition organised to test students' skills
-and knowledge in Economics, Geography and Mathematics. The
+and knowledge in Economics. The
 event is designed to encourage students to learn more about
-economics, geography and maths.</p>
-                <p>The Economics, Geography and Maths Olympiad is attended by
-students from various levels, ranging from elementary to
-University Levels. The participants will be tested with various
-questions related to the basic concepts of economics, geography
-and mathematics in everyday life.</p>
+economics.</p>
+                <p>The Economics Olympiad is attended by
+students from Senior High School levels. The participants will be tested with various
+questions related to the basic concepts of economics in everyday life.</p>
                 {/* <a href="/AboutPage"><button className="about-btn">Read More</button></a> */}
             </div>
             <img src="./assets/images/logo/IYEO.png" alt="" />
